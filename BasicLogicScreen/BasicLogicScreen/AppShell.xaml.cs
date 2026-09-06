a7dfb55fec94bfd565d2546628217242d7f76aab
Cody Ackerman <codya1314@hotmail.com>
2026-09-06 17:34:49 -0400

    Added basic login screen functionality

---
+++ BasicLogicScreen/BasicLogicScreen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicLogicScreen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BasicLogicScreen/BasicLogicScreen/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace BasicLoginScreen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            string userName = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            if (userName == "Ackerman" && password == "Password1")
+            {
+                lblMessage.Text = "Login successful - " + userName;
+            }
+            else
+            {
+                lblMessage.Text = "Login failed - " + userName;
+            }
+        }
+
+        private void OnCancelClicked(object sender, EventArgs e)
+        {
+            txtUserName.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            lblMessage.Text = string.Empty;
+        }
+    }
+}
